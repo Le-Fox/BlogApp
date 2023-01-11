2313e6203eb9050c778a438fda7cb6629fd9a3f5
Le-Fox <smkupa@gmail.com>
2023-01-11 17:05:24 +0200

    fixed module issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 
 import { SessionProvider } from 'next-auth/react';
-import { AppProps } from 'next/app';
+//import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }/*: AppProps*/) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
